a973b4cd56932895764616e5b3ac5ec83b17ac5d
Vds <144252754+DsValentin@users.noreply.github.com>
2024-03-26 12:33:11 +0100

    Update App.js

--- Application-web-Kasa/src/App.js
+++ Application-web-Kasa/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeaderComponent from "./components/header/header.component";
 import FooterComponent from "./components/footer/footer.component";
-import HomePage from "./pages/HomePage/homepage"; // Correction de l'importation
-import About from "./pages/AboutPage/aboutpage.component";
-import LogementPage from "./pages/logementpage/logementpage";
-import Error404 from "./pages/ErrorPage/errorpage";
+import HomePage from "./pages/HomePage/Homepage"; // Correction de l'importation
+import About from "./pages/AboutPage/Aboutpage.component";
+import LogementPage from "./pages/Logementpage/logementpage";
+import Error404 from "./pages/ErrorPage/Errorpage";
 
 function App() {
   return (
